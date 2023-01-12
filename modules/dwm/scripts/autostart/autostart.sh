9befbd0b3610516e726370f10e5cb4055f1595d3
FrengerH <frengerh@hfmail.nl>
2023-01-12 12:05:17 +0100

    nixos

--- modules/dwm/scripts/autostart/autostart.sh
+++ modules/dwm/scripts/autostart/autostart.sh
@@ -3,7 +3,6 @@
 check=`ps -e | command -v work | wc -l`
 echo $check
 if [ $check -gt 0 ]; then
-    echo "Test"
     dwm-msg run_command view 128
 else
     xrandr --newmode "1912x988_60.00"  156.64  1912 2024 2232 2552  988 989 992 1023  -HSync +Vsync
@@ -11,7 +10,7 @@ else
     xrandr --output Virtual-1 --mode 1912x988_60.00
 fi
 
-feh --bg-fill /etc/wallpaper/wallpaper.jpg
 sleep 1
+feh --bg-fill /etc/wallpaper/wallpaper.jpg
 dwmblocks
 
