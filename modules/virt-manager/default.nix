814a19acc6b8c00434aa70715f42de52db85b3a2
FrengerH <frengerh@hfmail.nl>
2023-01-14 01:20:44 +0100

    nixos

--- modules/virt-manager/default.nix
+++ modules/virt-manager/default.nix
@@ -1,16 +1,34 @@
 { config, pkgs, ... }:
 
 let
+  nixos-vm = pkgs.writeShellScriptBin "nixos-vm" ''
+    state=`virsh dominfo nixos | grep 'State:' | awk '{ print $2 }'`
+    if [ "$state" != "running" ]; then
+      virsh start nixos
+    fi
+    virt-viewer -c virt-viewer -c qemu:///session --domain-name nixos -f -w
+  '';
+  nixos-vm-desktop = (pkgs.makeDesktopItem {
+    name = "nixos-vm";
+    desktopName = "Nixos vm";
+    exec = "nixos-vm";
+  });
 in
   {
     boot.kernelModules = [ "kvm-intel" ];
     virtualisation.libvirtd.enable = true;
+    # virtualisation.libvirtd.extraConfig = ''
+    #   unix_sock_group = "libvirtd"
+    #   unix_sock_rw_perms = "0770"
+    # '';
     environment.systemPackages = with pkgs; [
       virt-manager
       virt-viewer
       dconf
+      nixos-vm
+      nixos-vm-desktop
     ];
-    
+
     users.users.${config.defaultUser}.extraGroups = [ "libvirtd" ];
   }
 
