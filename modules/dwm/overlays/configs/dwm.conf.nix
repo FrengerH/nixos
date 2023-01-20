9873f368bdc2e2c843f1c4cb8a83bb5148bd0609
FrengerH <frengerh@hfmail.nl>
2023-01-20 17:10:39 +0100

    nixos

--- modules/dwm/overlays/configs/dwm.conf.nix
+++ modules/dwm/overlays/configs/dwm.conf.nix
@@ -112,6 +112,8 @@ in {
         /* symbol     arrange function */
         { "[]=",      tile },    /* first entry is default */
         { "[M]",      monocle },
+        { "TTT",      bstack },
+	    { "===",      bstackhoriz },
         /*{ "><>",      NULL },*/    /* no layout function means floating behavior */
     };
 
