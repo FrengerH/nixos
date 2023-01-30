6fd9498f8bdf4278ec039cdd2002f7e7719a6db5
FrengerH <frengerh@hfmail.nl>
2023-01-30 21:09:15 +0100

    nixos

--- modules/dwm/scripts/dwmblocks/battery.sh
+++ modules/dwm/scripts/dwmblocks/battery.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-proc=`acpi -b | awk '{print $4}'`
-ICON=""
-printf "$ICON %s" "$proc"
+acpi=`ps -e | command -v acpi | wc -l`
+if [ $acpi -gt 0 ]; then
+    proc=`acpi -b | awk '{print $4}'`
+    ICON=""
+    printf "$ICON %s" "$proc"
+fi
+
