6936f8a263e97e869d4977f51afc2c674e168402
FrengerH <frengerh@hfmail.nl>
2023-10-05 17:37:40 +0200

    Nixos

--- modules/neovim/overlays/neovim/config/keymaps.lua
+++ modules/neovim/overlays/neovim/config/keymaps.lua
@@ -25,7 +25,7 @@ vim.keymap.set("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu(
 vim.keymap.set("n", "<C-j>", function() require("harpoon.ui").nav_file(1) end)
 vim.keymap.set("n", "<C-k>", function() require("harpoon.ui").nav_file(2) end)
 vim.keymap.set("n", "<C-l>", function() require("harpoon.ui").nav_file(3) end)
-vim.keymap.set("n", "<C-;>", function() require("harpoon.ui").nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(4) end)
 
 -- window movement remaps
 -- keymap("n", "<a-h>", "<cmd>wincmd h<cr>")
